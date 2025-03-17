c3b078dc39d71275844f00676eee262d92a29194
Hemanth16-gowda <hemanthgowdagc99@gmail.com>
2025-03-17 16:49:08 +0530

    Fix blank page issue by setting homepage

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',  // Fix for blank page issue on Vercel
   server: {
     host: '0.0.0.0',
     proxy: {
